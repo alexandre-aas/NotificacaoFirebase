d9104b7f4712631027b069a03eb400f01e7e2c9a
Alexandre <aas@unochapeco.edu.br>
2019-01-30 09:23:00 -0200

    inclusao dos fontes

---
+++ app/src/main/java/com/s/d/a/a/notificacaofirebase/NotificacaoFirebaseActivity.java
@@ -0,0 +1,13 @@
+package com.s.d.a.a.notificacaofirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotificacaoFirebaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notificacao_firebase);
+    }
+}
